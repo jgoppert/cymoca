eee92b7d1d5848fe5a14121e31b86b6b6b211b2b
James Goppert <james.goppert@gmail.com>
2018-03-18 05:18:54 -0400

    Fix for version.

--- src/cymoca_compiler/version.h
+++ src/cymoca_compiler/version.h
@@ -6,5 +6,8 @@
 //Global version strings
 extern const std::string VERSION;
 extern const std::string VERSION_SHORT;
+extern const std::string VERSION_MAJOR;
+extern const std::string VERSION_MINOR;
+extern const std::string VERSION_PATCH;
 
 #endif //VERSION_H
