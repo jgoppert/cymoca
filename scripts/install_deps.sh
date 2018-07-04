23a9eb49a4ebe2a9ba361fa9b63f6dfc73535949
James Goppert <james.goppert@gmail.com>
2018-07-04 02:25:22 -0400

    Switch to gcc-8, make install less verbose.

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,36 +3,36 @@
 set -e 
 
 # casadi
-mkdir -p /tmp/casadi
-pushd /tmp/casadi
-wget https://github.com/jgoppert/casadi/archive/3.4.4-jmg.tar.gz
-tar xzvf 3.4.4-jmg.tar.gz
-pushd casadi-3.4.4-jmg
-mkdir -p build
-pushd build
-cmake ..
-make -j4
-cpack -G DEB
+mkdir -p /tmp/casadi > /dev/null
+pushd /tmp/casadi > /dev/null
+wget https://github.com/jgoppert/casadi/archive/3.4.4-jmg.tar.gz &> download.log
+tar xzvf 3.4.4-jmg.tar.gz &> extract.log
+pushd casadi-3.4.4-jmg > /dev/null
+mkdir -p build > /dev/null
+pushd build > /dev/null
+cmake .. &>> build.log
+make -j4 &>> build.log
+cpack -G DEB &>> build.log
 sudo dpkg -i *.deb
-popd 
-popd
-popd
+popd > /dev/null
+popd > /dev/null
+popd > /dev/null
 
 # antlr4
-mkdir -p /tmp/antlr4
-pushd /tmp/antlr4
-wget https://github.com/jgoppert/antlr4/archive/4.7.1-cpack.tar.gz
-tar xzvf 4.7.1-cpack.tar.gz
-pushd antlr4-4.7.1-cpack/runtime/Cpp
-mkdir -p build
-pushd build
-cmake ..
-make -j4
-cpack -G DEB
+mkdir -p /tmp/antlr4 > /dev/null
+pushd /tmp/antlr4 > /dev/null
+wget https://github.com/jgoppert/antlr4/archive/4.7.1-cpack.tar.gz &> download.log
+tar xzvf 4.7.1-cpack.tar.gz &> extract.log
+pushd antlr4-4.7.1-cpack/runtime/Cpp > /dev/null
+mkdir -p build > /dev/null
+pushd build &>> /dev/null
+cmake .. &>> build.log
+make -j4 &>> build.log
+cpack -G DEB &>> build.log
 sudo dpkg -i *.deb
-popd 
-popd
-popd
+popd > /dev/null
+popd > /dev/null
+popd > /dev/null
 
 
 
