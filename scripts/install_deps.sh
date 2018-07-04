3cf26f7189e94f5422b70b0f2b0adda007371cef
James Goppert <james.goppert@gmail.com>
2018-07-04 02:52:47 -0400

    Make install script more verbose.

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -3,42 +3,42 @@
 set -e 
 
 # casadi
-mkdir -p /tmp/casadi > /dev/null
-pushd /tmp/casadi > /dev/null
+mkdir -p /tmp/casadi
+pushd /tmp/casadi
 echo downloading casadi
-wget https://github.com/jgoppert/casadi/archive/3.4.4-jmg.tar.gz &> download.log
-tar xzvf 3.4.4-jmg.tar.gz &> extract.log
-pushd casadi-3.4.4-jmg > /dev/null
-mkdir -p build > /dev/null
-pushd build > /dev/null
+wget https://github.com/jgoppert/casadi/archive/3.4.4-jmg.tar.gz > download.log
+tar xzvf 3.4.4-jmg.tar.gz > extract.log
+pushd casadi-3.4.4-jmg
+mkdir -p build
+pushd build
 echo building casadi
-cmake .. &>> build.log
-make -j4 &>> build.log
+cmake ..
+make -j4
 echo packiaging casadi
-cpack -G DEB &>> build.log
+cpack -G DEB
 sudo dpkg -i *.deb
-popd > /dev/null
-popd > /dev/null
-popd > /dev/null
+popd
+popd
+popd
 
 # antlr4
-mkdir -p /tmp/antlr4 > /dev/null
-pushd /tmp/antlr4 > /dev/null
+mkdir -p /tmp/antlr4
+pushd /tmp/antlr4
 echo downloading antlr4
-wget https://github.com/jgoppert/antlr4/archive/4.7.1-cpack.tar.gz &> download.log
-tar xzvf 4.7.1-cpack.tar.gz &> extract.log
-pushd antlr4-4.7.1-cpack/runtime/Cpp > /dev/null
-mkdir -p build > /dev/null
-pushd build &>> /dev/null
+wget https://github.com/jgoppert/antlr4/archive/4.7.1-cpack.tar.gz > download.log
+tar xzvf 4.7.1-cpack.tar.gz > extract.log
+pushd antlr4-4.7.1-cpack/runtime/Cpp
+mkdir -p build
+pushd build
 echo building antlr4
-cmake .. &>> build.log
-make -j4 &>> build.log
+cmake ..
+make -j4
 echo packaging antlr4
-cpack -G DEB &>> build.log
+cpack -G DEB
 sudo dpkg -i *.deb
-popd > /dev/null
-popd > /dev/null
-popd > /dev/null
+popd
+popd
+popd
 
 
 
